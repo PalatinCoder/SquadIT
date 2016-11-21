32e0e40686d5dff883303f0200b35300da522592
PalatinCoder <github@jan-sl.de>
2016-11-21 22:13:25 +0100

    FEATURE: Add authentication controller

---
+++ Classes/SquadIT/WebApp/Controller/AuthenticationController.php
@@ -0,0 +1,49 @@
+<?php
+namespace SquadIT\WebApp\Controller;
+
+/*
+ * This file is part of the SquadIT.WebApp package.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Mvc\ActionRequest;
+use TYPO3\Flow\Security\Authentication\Controller\AbstractAuthenticationController;
+
+class AuthenticationController extends AbstractAuthenticationController
+{
+
+    /**
+     * Displays a login form
+     *
+     * @return void
+     */
+    public function loginAction()
+    {
+    }
+
+    /**
+     * Logs all active tokens out and redirects the user to the login form
+     *
+     * @return void
+     */
+    public function logoutAction()
+    {
+        parent::logoutAction();
+        $this->addFlashMessage('Logout successful');
+        $this->redirect('login');
+    }
+
+    /**
+     * Will be triggered upon successful authentication
+     *
+     * @param ActionRequest $originalRequest The request that was intercepted by the security framework, NULL if there was none
+     * @return string
+     */
+    protected function onAuthenticationSuccess(ActionRequest $originalRequest = null)
+    {
+        if ($originalRequest !== null) {
+            $this->redirectToRequest($originalRequest);
+        }
+        $this->redirect('index', 'StandardController');
+    }
+}
