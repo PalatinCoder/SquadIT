a887dcd98486faf5f7f34e8d896581c9ed4fb147
PalatinCoder <github@jan-sl.de>
2016-12-09 18:45:17 +0100

    TASK: SquadController extends AbstractUserAwareActionController

--- Classes/SquadIT/WebApp/Controller/SquadController.php
+++ Classes/SquadIT/WebApp/Controller/SquadController.php
@@ -7,18 +7,13 @@ namespace SquadIT\WebApp\Controller;
 
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Persistence\Doctrine\PersistenceManager;
-use TYPO3\Flow\Security\Context;
 use TYPO3\Flow\Security\Account;
-use TYPO3\Flow\Security\AccountRepository;
 use TYPO3\Flow\Security\Policy\PolicyService;
-use TYPO3\Flow\Mvc\View\ViewInterface;
-use TYPO3\Flow\Mvc\Controller\ActionController;
 use SquadIT\WebApp\Domain\Model\Squad;
 use SquadIT\WebApp\Domain\Repository\SquadRepository;
 use SquadIT\WebApp\Domain\Model\User;
-use SquadIT\WebApp\Domain\Repository\UserRepository;
 
-class SquadController extends ActionController
+class SquadController extends AbstractUserAwareActionController
 {
     /**
      * @Flow\Inject
@@ -39,45 +34,6 @@ class SquadController extends ActionController
     protected $squadRepository;
 
     /**
-     * @Flow\Inject
-     * @var UserRepository
-     */
-    protected $userRepository;
-
-    /**
-     * @Flow\Inject
-     * @var AccountRepository
-     */
-    protected $accountRepository;
-
-    /**
-     * @Flow\Inject
-     * @var Context
-     */
-    protected $securityContext;
-
-    /**
-     * Holds the currently logged in user
-     *
-     * @var User
-     */
-    protected $user;
-
-    /**
-     * @param ViewInterface $view
-     * @return void
-     */
-    public function initializeView(ViewInterface $view)
-    {
-        $this->user = $this->userRepository->findOneByAccount($this->securityContext->getAccount());
-        if ($this->user === null) {
-            return;
-        }
-        $view->assign('user_firstname', $this->user->getFirstname());
-        $view->assign('user_initials', substr($this->user->getFirstname(), 0, 1) . substr($this->user->getLastname(), 0, 1));
-    }
-
-    /**
      * @param Squad $squad
      * @return void
      */
@@ -91,7 +47,6 @@ class SquadController extends ActionController
      */
     public function newAction()
     {
-        $this->view->assign('user_email', $this->securityContext->getAccount()->getAccountIdentifier());
     }
 
     /**
